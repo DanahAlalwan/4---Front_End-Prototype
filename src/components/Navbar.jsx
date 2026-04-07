013850c0b5e0270584c53759d3faa8cb32494740
DanahAlalwan <s202251380@kfupm.edu.sa>
2026-04-07 19:07:47 +0300

    Navbar Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav>
-      <h2>Nakhla</h2>
-      <Link to="/">Home</Link>
-      <Link to="/favorites">Favorites</Link>
-      <Link to="/login">Login</Link>
+    <nav className="navbar">
+      <div className="logo">Nakhla</div>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/category/Fashion">Categories</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/brand-owner">Brand Owner</Link>
+        <Link to="/admin">Admin</Link>
+      </div>
     </nav>
   )
 }
