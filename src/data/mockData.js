3611b4f2c89a61fa2c49bb9ae2ee8ffc0fbdae38
DanahAlalwan <s202251380@kfupm.edu.sa>
2026-04-07 18:48:57 +0300

    mockData Updated

---
+++ src/data/mockData.js
@@ -0,0 +1,19 @@
+
+export const brands = [
+  {
+    id: 1,
+    name: "Najd Perfumes",
+    category: "Perfume",
+    rating: 4.5,
+    image: "https://via.placeholder.com/200",
+    description: "Luxury Saudi perfumes"
+  },
+  {
+    id: 2,
+    name: "Safa Abayas",
+    category: "Abayas",
+    rating: 4.7,
+    image: "https://via.placeholder.com/200",
+    description: "Elegant abayas"
+  }
+]
