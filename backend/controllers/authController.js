fb1a1eb1064022804fe2a767ba5db1cb13e5042f
batool3-A <s202160170@kfupm.edu.sa>
2026-04-26 21:16:50 +0300

    errorMiddleware

---
+++ backend/controllers/authController.js
@@ -0,0 +1,78 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
+
+const registerUser = async (req, res) => {
+  const { name, email, password, role } = req.body
+
+  if (!name  !password) {
+    res.status(400)
+    throw new Error('Please provide name, email, and password')
+  }
+
+  if (password.length < 6) {
+    res.status(400)
+    throw new Error('Password must be at least 6 characters')
+  }
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  })
+
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id),
+  })
+}
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    res.status(400)
+    throw new Error('Please provide email and password')
+  }
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+}
+
+const getMe = async (req, res) => {
+  res.json(req.user)
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+}
