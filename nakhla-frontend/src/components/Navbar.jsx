e9e4c2bcf9db0a338c46fab9552231bb8d13471e
DanahAlalwan <s202251380@kfupm.edu.sa>
2026-04-26 17:02:01 +0300

    Navbar.jsx Updated

--- nakhla-frontend/src/components/Navbar.jsx
+++ nakhla-frontend/src/components/Navbar.jsx
@@ -1,18 +1,57 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
 
 export default function Navbar() {
+  const navigate = useNavigate()
+  const { user, logout } = useAppContext()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login')
+  }
+
   return (
     <nav className="navbar">
-      <div className="logo">Nakhla</div>
+      <Link to="/" className="logo">
+        Nakhla
+      </Link>
 
       <div className="nav-links">
         <Link to="/">Home</Link>
-        <Link to="/category/Fashion">Categories</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/brand-owner">Brand Owner</Link>
-        <Link to="/admin">Admin</Link>
+
+        {user.isAuthenticated && (
+          <>
+            <Link to="/category/beauty">Categories</Link>
+
+            {user.role === 'customer' && <Link to="/favorites">Favorites</Link>}
+
+            {user.role === 'brand-owner' && (
+              <>
+                <Link to="/brand-owner">Brand Owner</Link>
+                <Link to="/create-brand-profile">Create Brand</Link>
+              </>
+            )}
+
+            {user.role === 'admin' && (
+              <>
+                <Link to="/admin">Admin</Link>
+                <Link to="/verification-requests">Requests</Link>
+                <Link to="/manage-categories">Categories Management</Link>
+              </>
+            )}
+
+            <button className="nav-button" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        )}
+
+        {!user.isAuthenticated && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </>
+        )}
       </div>
     </nav>
   )
