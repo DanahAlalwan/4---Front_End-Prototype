de13ff259f039710033d4f2acc9ad1a278743298
Reham-ms <s20227010@kfupm.edu.sa>
2026-05-02 04:32:41 +0300

    Connect backend to MongoDB Atlas database

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,21 @@ dotenv.config()
 connectDB()
 
 const app = express()
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+]
 
-app.use(cors())
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
